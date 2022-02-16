3a6ee7c08d16e02a2f145e4394ef2c78c6423039
Adam Blomberg <blomadam@gmail.com>
2022-02-16 00:49:50 -0500

    updated older files for new data folder structure

--- src/autotest_data/load_data.py
+++ src/autotest_data/load_data.py
@@ -1,2 +1,62 @@
+import pandas as pd
+import configparser
+import psycopg2
+
+
+def collect_data(
+        study_filter_start: str ='2018-06-01',
+        study_filter_end: str = '2018-06-30'
+) -> None:
+    """
+    Connect to AACT database and download two files to data/00raw directory as *.csv.gz.
+    main_table.csv.gz has the studies with start_date between given dates (inclusive)
+    keywords_table.csv.gz has the keywords listed for the selected studies
+    :return:
+    """
+
+    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    config.read('credentials.ini')
+    connection_string = config["passwords"]["aact_db_connection_string"]
+    with psycopg2.connect(connection_string) as conn:
+        # collect main data table
+        df = pd.read_sql(
+            sql=f"""
+                select s.nct_id, s.start_date, s.verification_date, s.completion_date, s.completion_date_type,
+                s.study_type, s.brief_title, s.official_title, b.description, s.enrollment, s.enrollment_type
+                from studies as s
+                join brief_summaries as b on s.nct_id = b.nct_id
+                where (start_date >= '{study_filter_start}' AND
+                   start_date <= '{study_filter_end}')
+                order by nct_id;
+            """,
+            con=conn,
+        )
+        df.to_csv("data/00raw/main_table.csv.gz", index=False)
+
+        # collect keywords table
+        df = pd.read_sql(
+            sql=f"""
+                select nct_id, name
+                from keywords
+                where nct_id in (
+                    select nct_id
+                    from studies
+                    where (start_date >= '{study_filter_start}' AND
+                        start_date <= '{study_filter_end}')
+                )
+                order by nct_id;
+            """,
+            con=conn,
+        )
+        df.to_csv("data/00raw/keywords_table.csv.gz", index=False)
+
+
+
 def load_data(x: float) -> float:
-    return x*2
+    main_df = pd.read_csv("data/00raw/main_table.csv.gz")
+    keyword_df = pd.read_csv("data/00raw/keywords_table.csv.gz")
+    return main_df, keyword_df
+
+
+if __name__ == "__main__":
+    collect_data()
--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,14 +1,23 @@
 import pytest
 from autotest_data.load_data import load_data
+import hashlib
 
-
-@pytest.mark.parametrize("test_input,expected", [
-    (2, 4),
-    (-2, -4),
-    (100, 200)
+@pytest.mark.parametrize("test_file, expected_hash", [
+    # filenames and md5 hashes for files from sql queries
+    ("data/00raw/main_table.csv.gz", "ff7247b0119c3df7a1e593576c206c6f"),
+    ("data/00raw/keywords_table.csv.gz", "f8363838960fc63139d58324a43e11c6"),
 ])
-def test_data(test_input, expected):
-    assert load_data(test_input) is expected
+def test_data(test_file, expected_hash):
+    hash_buffer_length = 65536  # read files in 64kb chunks
+    md5_sum = hashlib.md5()
+    with open(test_file, 'rb') as f:
+        while True:
+            data = f.read(hash_buffer_length)
+            if not data:
+                break
+            md5_sum.update(data)
+    result = md5_sum.hexdigest()
+    assert result == expected_hash
 
 
 @pytest.mark.skip(reason="input checking not implemented")
